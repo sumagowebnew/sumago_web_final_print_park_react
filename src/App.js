6b2234f84606f2978d616aa9da263507dd1eb8b3
sahil0709 <a.sahil0709@gmail.com>
2024-03-12 17:15:41 +0530

    changes done in navbar for mobile view

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './main components/Home';
 import AboutUs from './main components/AboutUs';
-import Oursolutions from './main components/Oursolutions';
 import Footer from './main components/Footer';
 import Career from './main components/Career';
 import Contact1 from './main components/Contact1';
-import { data, services } from './main components/obnjectsfie'
+import { data } from './main components/obnjectsfie'
 import Oursolutions2 from './main components/Oursolutions2';
 function App() {
   return (
